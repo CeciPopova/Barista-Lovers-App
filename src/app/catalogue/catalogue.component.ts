03ff717d881caff40265fa317e35c60457062764
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2024-01-25 11:40:43 +0200

    fetch data from resources to the catalogue

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+import { Post } from '../types/post';
 
 @Component({
   selector: 'app-catalogue',
@@ -8,16 +10,20 @@ import { ApiService } from '../api.service';
 })
 export class CatalogueComponent implements OnInit{
 
+  themesList: Theme[] = [];
+  postsList: Post[] = [];
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe((themes) => {
       console.log({themes});
-
+      this.themesList = themes;
     })
 
     this.apiService.getPosts().subscribe((posts) => {
       console.log({posts});
+      this.postsList = posts;
     })
     
   }
