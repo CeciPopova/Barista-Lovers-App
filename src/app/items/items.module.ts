8c505b52ce6b501e960ceeaf1786e63a27a899db
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2024-02-01 22:10:02 +0200

    validation create new item with template-driven forms

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -4,12 +4,13 @@ import { ItemsRoutingModule } from './items-routing.module';
 import { NewItemComponent } from './new-item/new-item.component';
 import { EditItemComponent } from './edit-item/edit-item.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [NewItemComponent, EditItemComponent, ItemDetailsComponent],
   imports: [
-    CommonModule, ItemsRoutingModule
+    CommonModule, ItemsRoutingModule, FormsModule,
   ],
   exports: [NewItemComponent, EditItemComponent, ItemDetailsComponent]
 })
