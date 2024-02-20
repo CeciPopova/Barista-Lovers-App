e62cc5e03a7831f1660ad2a5edeaad250621955c
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2024-02-20 21:58:00 +0200

    fix authentication

--- src/app/authenticate/authenticate.component.ts
+++ src/app/authenticate/authenticate.component.ts
@@ -8,20 +8,20 @@ import { UserService } from '../user/user.service';
 })
 export class AuthenticateComponent implements OnInit{
 
-  isAuthenticated = true;
+  isAuthenticated = false;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.getProfile().subscribe({
       next: () => {
-        this.isAuthenticated = false;
+        this.isAuthenticated = true;
       },
       error: () => {
-        this.isAuthenticated = false;
+        this.isAuthenticated = true;
       },
       complete: () => {
-        this.isAuthenticated = false;
+        this.isAuthenticated = true;
       }
     })
   }
